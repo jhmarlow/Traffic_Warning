43425383e046ec6490ac42424f753c644fe49c20
jhmarlow1 <jhmarlow1@gmail.com>
2019-03-30 09:33:04 +0000

    addition of docstrings

--- gmap_api.py
+++ gmap_api.py
@@ -4,24 +4,45 @@ from datetime import datetime
 from threading import Timer
 
 def get_gmaps_trip_duration(start_point, end_point, api_key):
-    request_url = 'https://maps.googleapis.com/maps/api/directions/json?origin={}&destination={}&key={}'.format(start_point, end_point, api_key)
-    response = requests.get(request_url)
-    response_json = response.json()
-    duration = response_json['routes'][0]['legs'][0]['duration']['value']
+        """Get .json file from google maps api.
 
-    duration_inmins = duration/60
-    return duration_inmins
+        Arguments:
+                start_point {str} -- name of location
+                end_point {str} -- name of location
+                api_key {str} -- key  
+
+        return {float} - journey duration in mins
+        """
+        request_url = 'https://maps.googleapis.com/maps/api/directions/json?origin={}&destination={}&key={}'.format(start_point, end_point, api_key)
+        response = requests.get(request_url)
+        response_json = response.json()
+        duration = response_json['routes'][0]['legs'][0]['duration']['value']
+
+        duration_inmins = duration/60
+        return duration_inmins
 
 def post_ifttt_webhook(event, value):
-    ifttt_webhook_url = 'https://maker.ifttt.com/trigger/{}/with/key/yh6FJ6BvybjhYSFIt-R6L'
-    # payload to be sent to iftttt service
-    data = {'value1':value}
-    # insert our desired event
-    ifttt_event_url = ifttt_webhook_url.format(event)
-    # sends http post request to webhook url
-    requests.post(ifttt_event_url, json=data)
+        """Create webhook using IFTTT app.
+        
+        Arguments:
+                event {str} -- name of event create using IFTTT
+                value {float} -- duration of journey
+        """
+        ifttt_webhook_url = 'https://maker.ifttt.com/trigger/{}/with/key/yh6FJ6BvybjhYSFIt-R6L'
+        # payload to be sent to iftttt service
+        data = {'value1':value}
+        # insert our desired event
+        ifttt_event_url = ifttt_webhook_url.format(event)
+        # sends http post request to webhook url
+        requests.post(ifttt_event_url, json=data)
 
 def set_timer():
+        """Create timer for when to check gmaps.
+
+        Returns:
+                [type] -- [description]
+
+        """
         x=datetime.today()
         y=x.replace(day=x.day, hour=6, minute=30, second=0, microsecond=0)
         delta_t=y-x
@@ -29,6 +50,13 @@ def set_timer():
         return secs
 
 def main(start_point, end_point, acceptable_time_inmin):
+        """Run the main function.
+        
+        Arguments:
+                start_point {[type]} -- [description]
+                end_point {[type]} -- [description]
+                acceptable_time_inmin {[type]} -- [description]
+        """
         api_key = 'AIzaSyBHv9_oMQrc_SdgYa_ffaZ_wrxddXNBvBo'
         duration_inmins = get_gmaps_trip_duration(start_point, end_point, api_key)
         if acceptable_time_inmin < duration_inmins:
